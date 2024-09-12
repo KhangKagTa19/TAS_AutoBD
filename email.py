9bccdc433e4a1736b83ff03bca11e394f5f56f6b
KhangKagTa19 <khang.ta1902@hcmut.edu.vn>
2024-09-12 15:53:36 +0700

    UPLOAD src

--- email.py
+++ email.py
@@ -1,6 +1,7 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import streamlit as st
+import os
 
 def send_email(to_email, subject, content):
     message = Mail(
@@ -9,7 +10,14 @@ def send_email(to_email, subject, content):
         subject=subject,
         html_content=content)
     try:
-        sg = SendGridAPIClient('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')
+        # Prompt the user to input the SendGrid API key
+        sendgrid_api_key = input("Please enter your SendGrid API key: ")
+
+        # Set the key as an environment variable
+        os.environ['SENDGRID_API_KEY'] = sendgrid_api_key
+
+        # Initialize the SendGrid client
+        sg = SendGridAPIClient(api_key=os.environ['SENDGRID_API_KEY'])
         response = sg.send(message)
         return True, f"Email sent successfully. Status code: {response.status_code}"
     except Exception as e:
